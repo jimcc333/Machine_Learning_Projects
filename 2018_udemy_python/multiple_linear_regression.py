38b17beecba925aa025c0386e337601713bae890
jimcc333 <jimcc333@gmail.com>
2018-03-26 17:24:13 -0500

    builds the multiple linear regression model

--- 2018_udemy_python/multiple_linear_regression.py
+++ 2018_udemy_python/multiple_linear_regression.py
@@ -5,8 +5,8 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
-from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
 # Import data
 data_path = "D:\\Data Science\\2018_udemy_ml\\downloaded\\"
@@ -17,21 +17,16 @@ X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
 # Encode categorical data
-print(X)
 labeler_X = LabelEncoder()
 X[:, 3] = labeler_X.fit_transform(X[:, 3])
 encoder1 = OneHotEncoder(categorical_features=[3])
 X = encoder1.fit_transform(X).toarray()
-print(X)
-exit()
-
-# Scale data
-scaler_X = StandardScaler()
-X[:, 3:] = scaler_X.fit_transform(X[:, 3:])
-
-print("Features before splitting:\n", X)
-print("Observations before splitting:\n", y)
+X = X[:, 1:]    # Avoiding the dummy variable trap
 
 # Split data to training/test
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+# Build model
+regressor1 = LinearRegression()
+regressor1.fit(X_train, y_train)
 
