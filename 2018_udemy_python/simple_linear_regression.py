14dbf4dc81eff2a008000425568983ee535edeb8
jimcc333 <jimcc333@gmail.com>
2018-03-26 16:55:19 -0500

    plots the simple linear regression model and distinguishes training/test data

--- 2018_udemy_python/simple_linear_regression.py
+++ 2018_udemy_python/simple_linear_regression.py
@@ -4,12 +4,9 @@
 
 # Import libraries
 import pandas as pd
-from sklearn.preprocessing import Imputer
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
 
 # Import data
 data_path = "D:\\Data Science\\2018_udemy_ml\\downloaded\\Simple_Linear_Regression\\"
@@ -31,5 +28,14 @@ for index in range(len(y_test)):
     print(round(100*abs(y_test[index] - y_predicted[index])/(y_predicted[index]), 2),
           end=', ')
 print()
+
+# Plot model
+plt.scatter(X_train, y_train, color='blue', label='Training Data')
+plt.scatter(X_test, y_test, color='red', marker='x', label='Test Data')
+plt.plot(X_train, regressor1.predict(X_train), label='Fit Line')
+plt.xlabel("Years of Experience")
+plt.ylabel("Salary, $")
+plt.legend()
+plt.show()
 
 print("\n\nEnd of script.\n")
