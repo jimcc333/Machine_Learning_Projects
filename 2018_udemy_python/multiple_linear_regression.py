36df4f6cdffd50f8bf25f1f2f706175ebad1ba3f
jimcc333 <jimcc333@gmail.com>
2018-03-26 17:19:40 -0500

    adds the categorical variable encoding workflow

--- 2018_udemy_python/multiple_linear_regression.py
+++ 2018_udemy_python/multiple_linear_regression.py
@@ -1,2 +1,37 @@
 """ Multiple Linear Regression Model. """
+# Author: Cem Bagdatlioglu
+
+# Import libraries
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+# Import data
+data_path = "D:\\Data Science\\2018_udemy_ml\\downloaded\\"
+dataset = pd.read_csv(data_path + "50_Startups.csv")
+print("Original Data:\n", dataset, "\n")
+
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# Encode categorical data
+print(X)
+labeler_X = LabelEncoder()
+X[:, 3] = labeler_X.fit_transform(X[:, 3])
+encoder1 = OneHotEncoder(categorical_features=[3])
+X = encoder1.fit_transform(X).toarray()
+print(X)
+exit()
+
+# Scale data
+scaler_X = StandardScaler()
+X[:, 3:] = scaler_X.fit_transform(X[:, 3:])
+
+print("Features before splitting:\n", X)
+print("Observations before splitting:\n", y)
+
+# Split data to training/test
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
